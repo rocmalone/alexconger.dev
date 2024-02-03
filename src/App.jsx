e04ee2b4e6c14c230e1213cd83326ace6b645887
rocmalone <114255759+rocmal1@users.noreply.github.com>
2024-02-02 22:18:13 -0800

    Add contact links

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,6 @@ function App() {
   const projectsDivRef = useRef(null);
   const resumeDivRef = useRef(null);
 
-  useEffect(() => {
-    // setRouteHash(window.location.hash);
-  }, []);
-
-  // useEffect(() => {
-  //   window.location.hash = routeHash;
-  // }, [routeHash]);
-
   // Scroll listener
   const checkIfDivInView = (targetDivRef) => {
     if (targetDivRef.current) {
@@ -152,18 +144,52 @@ function App() {
         <div className="headline-container">
           <h1>Hi. I'm Alex.</h1>
           <div className="headline">
-            I'm a <b>software developer</b> and <b>mechanical engineer</b>.
+            I'm a <b>software developer</b> and former{" "}
+            <b>mechanical engineer</b>.
           </div>
           <div className="headline">
             I've developed video games, written full-stack web apps,<br></br>
             designed electrical generators, and analyzed aircraft systems.
           </div>
           <div className="headline">
-            I love technical discussions and learning new things,<br></br>and I
+            I love technical discussions, learning new things,<br></br>and I
             believe good engineers ask lots of questions.
           </div>
           <div className="headline" style={{ marginTop: "30px" }}>
             Send me a message — let's solve your problem together.
+          </div>
+          <div>
+            <div className="logos">
+              <a
+                className="mail-icon"
+                href="mailto:aconger00@gmail.com"
+                target="_blank"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="auto"
+                  height="auto"
+                  fill="#033d9c"
+                  class="bi bi-envelope-at-fill mail-icon"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2zm-2 9.8V4.698l5.803 3.546zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.5 4.5 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586zM16 9.671V4.697l-5.803 3.546.338.208A4.5 4.5 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671" />
+                  <path d="M15.834 12.244c0 1.168-.577 2.025-1.587 2.025-.503 0-1.002-.228-1.12-.648h-.043c-.118.416-.543.643-1.015.643-.77 0-1.259-.542-1.259-1.434v-.529c0-.844.481-1.4 1.26-1.4.585 0 .87.333.953.63h.03v-.568h.905v2.19c0 .272.18.42.411.42.315 0 .639-.415.639-1.39v-.118c0-1.277-.95-2.326-2.484-2.326h-.04c-1.582 0-2.64 1.067-2.64 2.724v.157c0 1.867 1.237 2.654 2.57 2.654h.045c.507 0 .935-.07 1.18-.18v.731c-.219.1-.643.175-1.237.175h-.044C10.438 16 9 14.82 9 12.646v-.214C9 10.36 10.421 9 12.485 9h.035c2.12 0 3.314 1.43 3.314 3.034zm-4.04.21v.227c0 .586.227.8.581.8.31 0 .564-.17.564-.743v-.367c0-.516-.275-.708-.572-.708-.346 0-.573.245-.573.791" />
+                </svg>
+              </a>
+              <a href="https://www.linkedin.com/in/alexconger">
+                <img
+                  className="linkedin-logo"
+                  src="ext-logos/linkedin-logo.png"
+                ></img>
+              </a>
+              <a href="https://www.github.com/rocmalone">
+                <img
+                  className="github-logo"
+                  src="ext-logos/github-logo.svg"
+                ></img>
+              </a>
+            </div>
           </div>
         </div>
         <img className="suit-image" src="suit_cutout.png"></img>
@@ -245,83 +271,28 @@ function App() {
       <div id="skills" className="page" ref={skillsDivRef}>
         <h1>My Skills</h1>
         <div style={{ display: "flex" }}>
-          <div>
+          <div className="language-container">
             <h2 style={{ marginTop: "0" }}>Languages</h2>
-            <div
-              style={{
-                marginLeft: "1rem",
-                minWidth: "200px",
-              }}
-            >
-              {/* <div style={{ marginBottom: "1rem", fontSize: "0.9rem" }}>
-                <div style={{ marginRight: "1rem" }}>
-                </div>
-                <div style={{ marginRight: "1rem" }}>★☆☆ = Practical</div>
-                <div style={{ marginRight: "1rem" }}>★★☆ = Proficient</div>
-                <div style={{ marginRight: "1rem" }}>★★★ = Intermediate</div>
-              </div> */}
-              <table border="0" style={{}}>
-                <tbody>
-                  <tr>
-                    <td>JavaScript &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-                    <td>★★★</td>
-                  </tr>
-                  <tr>
-                    <td className="mini-td" style={{ paddingLeft: "1rem" }}>
-                      React
-                    </td>
-                    <td className="mini-td">★★★</td>
-                  </tr>
-                  <tr>
-                    <td className="mini-td" style={{ paddingLeft: "1rem" }}>
-                      TypeScript
-                    </td>
-                    <td className="mini-td">★★☆</td>
-                  </tr>
-                  <tr>
-                    <td className="mini-td" style={{ paddingLeft: "1rem" }}>
-                      Angular
-                    </td>
-                    <td className="mini-td">★★☆</td>
-                  </tr>
-                  <tr>
-                    <td>Java</td>
-                    <td>★★★</td>
-                  </tr>
-                  <tr>
-                    <td>Python</td>
-                    <td>★★★</td>
-                  </tr>
-                  <tr>
-                    <td>C#</td>
-                    <td>★★☆</td>
-                  </tr>
-                  <tr>
-                    <td>SQL</td>
-                    <td>★★☆</td>
-                  </tr>
-                  <tr>
-                    <td>C++</td>
-                    <td>★★☆</td>
-                  </tr>
-                  <tr>
-                    <td>MATLAB</td>
-                    <td>★☆☆</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <div style={{ fontSize: "0.9rem" }}>When I build things I use:</div>
+            <ul>
+              <li>JavaScript</li>
+              <li>Java</li>
+              <li>Python</li>
+              <li>C#</li>
+              <li>SQL</li>
+              <li>C++</li>
+              <li>MATLAB</li>
+            </ul>
           </div>
           {/* <img className="pointer-image" src="pointer.png"></img> */}
 
           <div
-            style={{ marginLeft: "30px" }}
+            style={{ marginLeft: "60px" }}
             className="skills-container skills-technology"
           >
             <h2 style={{ marginTop: "0" }}>Technologies</h2>
             <div style={{ marginBottom: "1rem", fontSize: "0.9rem" }}>
-              I am comfortable using, deploying with, and troubleshooting all of
-              the technologies listed here.
+              I am comfortable using, deploying with, and troubleshooting:
             </div>
             <table border="0">
               <tbody>
@@ -350,7 +321,7 @@ function App() {
                       <div className="bar"></div>
                     </div>
                   </td>
-                  <td>Azure, AWS, Linux VPS, Containers</td>
+                  <td>Azure, Linux VPS, Containers</td>
                 </tr>
                 <tr>
                   <td>
@@ -368,7 +339,7 @@ function App() {
                       <div className="bar"></div>
                     </div>
                   </td>
-                  <td>Ubuntu, Debian, PopOS, Windows</td>
+                  <td>Linux (especially Ubuntu/Debian), Windows</td>
                 </tr>
                 <tr>
                   <td>
@@ -389,7 +360,8 @@ function App() {
 
       <div id="resume" className="page" ref={resumeDivRef}>
         <h1>Resume</h1>
-        <div>I'm happy that you're interested in working with me.</div>
+        <p>.</p>
+        <p></p>
       </div>
     </div>
   );
